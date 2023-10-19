1ca8a5b4e3927b29a6542b953a53f686431f9011
wmason1997 <williamcmason1997@gmail.com>
2023-10-18 23:08:53 -0700

    added the four sets to use in generation

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log('clicked');
+}
+
+
 // Write password to the #password input
 function writePassword() {
   // window.prompt("You clicked me!");
@@ -35,6 +40,21 @@ function writePassword() {
     return; // breaks out of pw generator
   }
 
+  var numeric_set = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var lowercase_set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var uppercase_set = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  // var special_set = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+  var usable_set = [];
+
+// add concat code
+
+
+
+
 
 
 
@@ -53,4 +73,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
