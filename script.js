f5d1fecb59e91a74fcf60784f0cb510765661e13
wmason1997 <williamcmason1997@gmail.com>
2023-10-21 20:10:35 -0700

    Changed the prompt responses in the window to more closely resemble what was shown on example.

--- script.js
+++ script.js
@@ -14,46 +14,44 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-window.alert("You clicked me!"); // update later
+// window.alert("You clicked me!"); // update later
 
   var password_to_be_returned = ''; // had to move inside function so that a new empty password string is initialized every time
   var selected_subset = []; // predefining the set that will have 1-4 sets later
   // similarly had to move inside function so that it would be reset to empty every time button is clicked
 
 
-  var user_desired_length = window.prompt("How long do you want your password to be? It needs to be at least 8 long and no more than 128 long.");
+  var user_desired_length = window.prompt("How many characters would you like your password to contain? It needs to be at least 8 long and no more than 128 long.");
 
   if (user_desired_length >= 8 && user_desired_length <= 128){
-    window.alert("Great"); }
+    // window.alert("Great"); // commented out. Don't need a return statement here. Just need to not return.
+    }
   else if (user_desired_length < 8){
     window.alert("Your password needs to be at least 8 characters long. Try again.");
-    return; }
+    return; } // exit out of prompt sequence
   else if (user_desired_length > 128){
     window.alert("Your password needs to be no more than 128 characters long. Try again");
-    return; }
+    return; } // exit out of prompt sequence
   else {
     window.alert("Your input was not numeric. Try again.")
-    return;
+    return; // exit out of prompt sequence
   }
 
 
   // Get lowercase characters boolean
-  var special_include = window.confirm("Click OK to confirm including special characters");
+  var special_include = window.confirm("Click OK to confirm including special characters.");
 
   // Get numeric characters boolean
-  var numeric_include = window.confirm("Click OK to confirm including numeric characters");
+  var numeric_include = window.confirm("Click OK to confirm including numeric characters.");
 
   // Get lowercase characters boolean
-  var lowercase_include = window.confirm("Click OK to confirm including lowercase characters");
+  var lowercase_include = window.confirm("Click OK to confirm including lowercase characters.");
 
   // Get uppercase characters boolean
-  var uppercase_include = window.confirm("Click OK to confirm including uppercase characters");
+  var uppercase_include = window.confirm("Click OK to confirm including uppercase characters.");
 
   // verifies that one character set was confirmed
-  // 
   if (special_include || numeric_include || lowercase_include || uppercase_include) {
-    window.alert("at least one was selected");
-
     if (numeric_include) {selected_subset = selected_subset.concat(numeric_set);}
     if (uppercase_include) {selected_subset = selected_subset.concat(uppercase_set);}
     if (lowercase_include) {selected_subset = selected_subset.concat(lowercase_set);}
@@ -94,19 +92,16 @@ window.alert("You clicked me!"); // update later
 
     window.alert("The generated password is: " + password_to_be_returned)
 
-  } else {
-    window.alert("None were selected :((( Try again ");
+  } else { // None of the four possible sets were selected
+    window.alert("No character sets were selected. Try again.");
     return; // breaks out of pw generator
   }
-
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   // passwordText.value = password;
-  passwordText.value = password_to_be_returned; // updated the updated password to be the one my function created
-
-
+  passwordText.value = password_to_be_returned; // Changed the updated password to be the one my function created. Did not need to do more DOM traversing than what is set up. 
 }
 
 // Add event listener to generate button
